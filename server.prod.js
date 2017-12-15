5ce5a26805c056f0ad765e4d64cc10f7c5f24f49
sfilippenko <sfilippenko@tsconsulting.ru>
2017-12-15 13:52:14 +0300

    heroku

---
+++ server.prod.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = new express();
+const port = 8080;
+
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(port, function (error) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('you are listening to port', port);
+    }
+});
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -14,7 +14,6 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/static/'
     },
     module: {
         rules: [
