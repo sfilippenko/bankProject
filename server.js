6414d594863b5d9d2bdf374d99a672af185bd9d5
sfilippenko <sfilippenko@tsconsulting.ru>
2018-08-31 18:48:17 +0300

    heroku script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 
 app.use(express.static('dist'));
 
